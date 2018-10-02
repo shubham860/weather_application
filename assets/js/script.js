fe3c53b4d9f991f1a4f4a494ad12d5d10f35f4a0
@shubham860 <shubham.chauhan860@gmail.com>
2018-10-03 02:34:50 +0530

    update-8

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,31 @@ var request = new XMLHttpRequest();
 request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status == 200) {
        var data = JSON.parse(request.responseText);
-       
-       var tempr = data.query.results.item.condition.temp;
+          console.log(data);
+       var tempr = data.query.results.channel.item.condition.temp;
               tempr=(5*(tempr-32))/9;
-          document.getElementById('temp').innerHTML= 'Temperature:' +  Math.floor(tempr) + '&deg;'+'C';
+       var wind = data.query.results.channel.wind.speed;
+       var humidity = data.query.results.channel.atmosphere.humidity;
+       var forecast = data.query.results.channel.item.forecast;
+          var high = forecast[0].high;
+          high=(5*(high-32))/9;
+          var low=forecast[0].low;
+          low=(5*(low-32))/9;
+        var city =  data.query.results.channel.location.city;
+        var rise =  data.query.results.channel.astronomy.sunrise;
+        var set =  data.query.results.channel.astronomy.sunset;
+
+          document.getElementById('city').innerHTML=  city;
+          document.getElementById('temp').innerHTML=  Math.floor(tempr) + '&deg;'+'C';
+          document.getElementById('wind').innerHTML=  Math.floor(wind) + 'mph';
+          document.getElementById('humidity').innerHTML=  Math.floor(humidity) + '%';
+          document.getElementById('high').innerHTML=  Math.floor(high) + '&deg;'+'C';
+          document.getElementById('low').innerHTML=  Math.floor(low) + '&deg;'+'C';
+          document.getElementById('rise').innerHTML= "Sunrise  -"+" "+rise;
+          document.getElementById('set').innerHTML= "Sunset  -"+" "+set;
+
+
+
     }
   };
   request.open('GET',url,true);
